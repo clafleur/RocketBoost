a2527b17de941ac71a0c4edf50d1a48364a1b431
clafleur <clafleur@users.noreply.github.com>
2018-02-08 08:23:14 -0800

    Added sound and speed
    Added a speed variable for the rotation
    added a sound for thrusters

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour {
-
+    [SerializeField]
+    private float speed = 5;
+    private AudioSource audioSource;
     private Rigidbody rigidBody;
 	// Use this for initialization
 	void Start ()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -22,15 +25,23 @@ public class Rocket : MonoBehaviour {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward * Time.deltaTime);
+            transform.Rotate(Vector3.forward * speed * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward * Time.deltaTime);
+            transform.Rotate(-Vector3.forward * speed * Time.deltaTime);
         }
     }
 }
